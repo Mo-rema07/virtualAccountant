ffb6495c52cbaf32894e18cee22e76dea7c2818a
Morema <25913934+Mo-rema07@users.noreply.github.com>
2019-11-14 00:35:40 +0400

    can make record on database

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,9 @@
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
+const admin = require('firebase-admin');
+admin.initializeApp();
+let db = admin.firestore();
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
@@ -13,8 +16,8 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
+  function welcome (agent) {
+    agent.add(`Welcome to your own personal virtual assistant! How can I help you?`);
   }
 
   function fallback(agent) {
@@ -22,63 +25,50 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     agent.add(`I'm sorry, can you try again?`);
   }
 
-  function transactionHandler(agent){
-    const amount = agent.paramaters.unit-currency
-    const action = agent.paramaters.action
-    const accounts = agent.paramaters.accounts
-    const date = agent.paramaters.date
-    if (! amount){
-      agent.add("What was the total amount?")
+  function recordTransaction(agent){
+    // console.log(agent.parameters)
+    // const date = agent.parameters.date
+    // const amount = agent.parameters.unit_currency
+    // const action = agent.parameters.Action
+    // const account = agent.parameters.Account
+
+    // const record = {
+    //   'date' : date,
+    //   'amount': amount,
+    //   'action': action,
+    //   'account': account
+    // }
+    const record = {
+      date : 'Today',
+      amount: '5 USD',
+      action: 'sold',
+      account: 'books'
     }
-    else if(! action){
-      agent.add("Say whether you bought or sold")
-    }
-    else if(! accounts){
-      agent.add("What did you buy ${action}?")
-    }
-    else if(! date){
-      agent.add("When did the transaction occur?")
-    }
-    else {
-      agent.add('Action, Accounts, Amount, Date')
-    }
+
+    let transRef = db.collection('transactions').doc('transaction1');
+
+    let setRecord = transRef.set(record)
+
+    console.log(record)
+    // db.collection('transactions').add(record).then(r => console.log(r))
+    let docRef = db.collection('transactions').doc('1stRecord')
+    // let setRecord = docRef.set(record)
+    agent.add(`Recording new transaction`)
+    return db.runTransaction(
+      transaction => {
+        transaction.set(docRef,record)
+      })
+      .then(() => {
+        return "write success!"
+      })
   }
-
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
-
-  // // Uncomment and edit to make your own Google Assistant intent handler
-  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function googleAssistantHandler(agent) {
-  //   let conv = agent.conv(); // Get Actions on Google library conv instance
-  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-  // }
-  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
-  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
 
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
-  intentMap.set('Record Transaction', transactionHandler);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
+  intentMap.set('Record transaction', recordTransaction);
   agent.handleRequest(intentMap);
+  // recordTransaction(agent);
+
 });
